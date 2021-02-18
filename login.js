7d8f1ee29cdb2c8d18ff67e336d18ac5032ae75e
Marina240490 <marina_schwab@yahoo.de>
2021-02-18 15:22:07 +0100

    added Responsive Menu to all Pages

--- login.js
+++ login.js
@@ -1,3 +1,30 @@
+let user = [
+    {
+        'name': 'Gast',
+        'password': 'Gast123!',
+        'image': 'img/user-pic.jpg',
+        'e-mail': 'gast@join.de',
+    },
+    {
+        'name': 'Marina',
+        'password': 'Marina123!',
+        'image': 'img/arina.jpg',
+        'e-mail': 'marina@join.de',
+    },
+    {
+        'name': 'Yvonne',
+        'password': 'Yvonne123!',
+        'image': 'img/yvonne.jpg',
+        'e-mail': 'Yvonne@join.de',
+    },
+    {
+        'name': 'Steffi',
+        'password': 'Steffi123!',
+        'image': 'img/steffi.jpg',
+        'e-mail': 'Steffi@join.de',
+    }
+];
+
 /**
  * Login function to enter the main page
  */
