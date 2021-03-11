ef50f0669ff207fafd01bbcbf471954d8e7b2889
Marina240490 <marina_schwab@yahoo.de>
2021-03-11 17:47:14 +0100

    overlay works together with required

--- addtask.js
+++ addtask.js
@@ -11,7 +11,7 @@ async function createTask(){ // Waiting for server response
       let date = document.getElementById('date');
       let category = document.getElementById('category');
       let urgency = document.getElementById('urgency');
-      let author = document.getElementById('user');
+      let author = document.getElementById('user-pic');
       let description = document.getElementById('description');
       let plususer = document.getElementById('plususer');
       
@@ -87,26 +87,22 @@ function closeOverlay() {
     document.getElementsByClassName('modal-container')[0].classList.add('hidden');
 }
 
+/**
+ * required and Overlay work together 
+ */
+function createTaskSubmit(event){
+    event.preventDefault();
+    console.log(event);
 
-/**function openOverlay() { 
-    let openOverlaySuccessful = false; // Open Overlay defnieren
-    
-        if (title.value > 0 && date.value > 0 && category.value > 0 && urgency.value > 0 && description.value > 0) { //Wenn Werte in allen Feldern vorhanden
-
-            openOverlaySuccessful = true; // Open Overlay als erfolgreich deklarieren
-            
-            document.getElementsByClassName('modal-container')[0].classList.remove('hidden');
-            document.getElementById('inhalt').classList.remove('hidden');
-            document.getElementById('textaddtask').innerHTML = 'Task wurde in Board geladen';
-        } 
-    
-        else { // Daten fehlen
-            document.getElementsByClassName('modal-container')[0].classList.remove('hidden');
-            document.getElementById('inhalt').classList.remove('hidden');
-            document.getElementById('textaddtask').innerHTML = 'Bitte ergänzen Sie die fehlenden Daten';
-        }
-}*/
-
+    createTask().then( () =>{
+        openOverlay();
+    })
+    .catch( error =>{
+        //handle create task error
+        console.error(new Error(error));
+    });
+    return false;
+}
 
 
 /**
